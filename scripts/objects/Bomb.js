b03ffac3c90e782a7444bb3fefea8901006b6b8f
Jakub Kaczmarek <jakub_kaczmarek@hotmail.com>
2017-09-21 16:04:36 +0200

    fix bomb

--- scripts/objects/Bomb.js
+++ scripts/objects/Bomb.js
@@ -1,13 +1,15 @@
 var Bomb = function (x, y, game, map) {
-    // this.flameGroup = game.add.group();
     this.map = map
     this.sprite = game.add.sprite(x, y, 'bomb', 'Bomb_f00');
-    this.alive = true;
-    this.objectBomb = this;
     this.sprite.animations.add('explode', Phaser.Animation.generateFrameNames('Bomb_f', 0, 2, '', 2), 1, true);
     this.sprite.animations.play('explode');
+
     game.physics.arcade.enable(this.sprite);
     this.sprite.body.immovable = true;
+
+    this.alive = true;
+    this.objectBomb = this;
+
     game.time.events.add(Phaser.Timer.SECOND * 3, this.destroyBomb, this);
 }
 
@@ -23,15 +25,14 @@ Bomb.prototype.createFlame = function (x, y) {
     game.state.states.Play.flameG.add(flame);
 }
 Bomb.prototype.destroyBomb = function () {
-    this.currentTileX = this.map.mapLayers['grass'].getTileX(this.sprite.x);
-    this.currentTileY = this.map.mapLayers['grass'].getTileY(this.sprite.y);
-    var tile = this.map.map.getTile(this.currentTileX, this.currentTileY);
-    this.createFlame(tile.worldX, tile.worldY);
+    this.currentTile = this.map.map.getTileWorldXY(this.sprite.x, this.sprite.y);
+    this.createFlame(this.currentTile.worldX, this.currentTile.worldY);
 
-    var above = this.map.map.getTileAbove(0, this.currentTileX, this.currentTileY);
-    var below = this.map.map.getTileBelow(0, this.currentTileX, this.currentTileY);
-    var left = this.map.map.getTileLeft(0, this.currentTileX, this.currentTileY);
-    var right = this.map.map.getTileRight(0, this.currentTileX, this.currentTileY);
+    var above = this.map.map.getTileAbove(0, this.currentTile.x, this.currentTile.y);
+
+    var below = this.map.map.getTileBelow(0, this.currentTile.x, this.currentTile.y);
+    var left = this.map.map.getTileLeft(0, this.currentTile.x, this.currentTile.y);
+    var right = this.map.map.getTileRight(0, this.currentTile.x, this.currentTile.y);
 
     this.tileAbove = this.map.map.getTile(above.x, above.y, 'collide');
     this.tileBelow = this.map.map.getTile(below.x, below.y, 'collide');
@@ -39,20 +40,16 @@ Bomb.prototype.destroyBomb = function () {
     this.tileRight = this.map.map.getTile(right.x, right.y, 'collide');
 
     if (this.tileAbove === null) {
-        var tile = this.map.map.getTile(above.x, above.y);
-        this.createFlame(tile.worldX, tile.worldY);
+        this.createFlame(above.worldX, above.worldY);
     }
     if (this.tileBelow == null) {
-        var tile = this.map.map.getTile(below.x, below.y);
-        this.createFlame(tile.worldX, tile.worldY);
+        this.createFlame(below.worldX, below.worldY);
     }
     if (this.tileLeft == null) {
-        var tile = this.map.map.getTile(left.x, left.y);
-        this.createFlame(tile.worldX, tile.worldY);
+        this.createFlame(left.worldX, left.worldY);
     }
     if (this.tileRight == null) {
-        var tile = this.map.map.getTile(right.x, right.y);
-        this.createFlame(tile.worldX, tile.worldY);
+        this.createFlame(right.worldX, right.worldY);
     }
 
     game.physics.arcade.enable(game.state.states.Play.flameG);
